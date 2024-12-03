0c0ff332ff61f868d4c7933ae7435378eb24cfc9
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-12-03 09:40:50 +0100

    fix fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,28 +4,18 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory as FakerFactory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixture implements FixtureGroupInterface
+class AppFixtures extends Fixture
 {
-    private $faker;
     private $hasher;
     private $manager;
 
     public function __construct(UserPasswordHasherInterface $hasher)
     {
-        $this->faker = FakerFactory::create('fr_FR');
-        $this->hasher = $hasher;
-
-    }
-
-    public static function getGroups(): array
-    {
-        return ['prod', 'test'];
-    }
+    $this->hasher = $hasher;
+}
 
     public function load(ObjectManager $manager): void
     {
