5d9cc7722dd49cf2546d56b96394c5d39cb782a4
glerique <gael.lerique@gmail.com>
2024-12-10 13:39:54 +0100

    [#60] Send invitation

--- src/Service/GroqService.php
+++ src/Service/GroqService.php
@@ -33,7 +33,7 @@ class GroqService
                 'Content-Type' => 'application/json'
             ],
             'json' => [
-                'model' => 'llama3-8b-8192',
+                'model' => 'llama-3.3-70b-versatile',
                 'messages' => $messages
             ]
         ]);
